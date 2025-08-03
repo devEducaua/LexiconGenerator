7d6a7b2c22b96500dd2c546c431a20ff5360aaca
devEducaua <schokito4@gmail.com>
2025-08-02 21:09:14 -0300

    refactor(controller): simplify controller, same functionality without desnecessary variable

--- src/web/wordsController.ts
+++ src/web/wordsController.ts
@@ -3,13 +3,8 @@ import generateWords from "../core/generateWords";
 import { bodyDto } from "./bodyDto";
 
 async function wordsController(c: Context) {
-    const { consonants, vowels, numberWords, maxLength, exclusions, struct } = await c.req.json();
-
-    const dto: bodyDto = { consonants, vowels, numberWords, maxLength, exclusions, struct };
-
-    const words: string[] = generateWords(dto);
-
-    return c.json(words);
+    const dto: bodyDto = await c.req.json();
+    return c.json(generateWords(dto));
 }
 
 export default wordsController;

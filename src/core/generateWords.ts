9037009eedaec0332e50eef177a9a08a6d3bbcfa
devEducaua <schokito4@gmail.com>
2025-08-07 23:48:24 -0300

    fix: change + to - on numSyllables in the function generateWords

--- src/core/generateWords.ts
+++ src/core/generateWords.ts
@@ -7,7 +7,7 @@ export default function generateWords(dto: bodyDto): string[] {
     const { consonants, vowels, numberWords, maxLength, minLength, exclusions, struct, rewrites } = dto;
 
     for (let i = 0; i < numberWords; i++) {
-        const numSyllables: number = Math.floor(Math.random() * (maxLength + minLength + 1)) + minLength;
+        const numSyllables: number = Math.floor(Math.random() * (maxLength - minLength + 1)) + minLength;
 
         let word: string = "";
 

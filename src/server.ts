e420b6005e6c5f6bdc305cb9e9d690113b80c8fe
devEducaua <schokito4@gmail.com>
2025-08-09 13:38:03 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono/tiny";
+import { cors } from "hono/cors";
 
 import wordsController from "./web/wordsController";
 
 const app = new Hono().basePath("/api");;
+
+app.use("*", cors());
 
 app.post('/', (c) => wordsController(c));
 
